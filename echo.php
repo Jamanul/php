e858633f1e85dacbd82e687eea53a99627a29ea3
jamanul <jk.skb.25@gmail.com>
2026-01-13 15:46:51 +0600

    first commit

---
+++ echo.php
@@ -0,0 +1,9 @@
+<?php
+echo "yahoo", "baba";
+echo "<h1>yahoo" . "baba</h1>";
+// print "yahoo", "baba"; 
+print "<h1>yahoo" . "baba</h1>";
+
+
+// comma doesn't work in print
+?>
---
+++ variable.php
@@ -0,0 +1,22 @@
+<!-- video no 5 -->
+
+
+<?php
+$a = "this is a variable";
+
+echo $a;
+$num = 25.87;
+
+echo $num;
+
+$firstName;
+$__firstName;
+
+// wrong way to write variable
+// $first Name;
+// $99firstName;
+// $first%Name;
+
+// $age and $AGE not same
+// as the php is case sensitive
+?>
