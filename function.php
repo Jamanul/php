ba3a12e6900db7042550b0c9460458944b74a074
jamanul <jk.skb.25@gmail.com>
2026-01-29 12:07:38 +0600

    resumed learning php

--- function.php
+++ function.php
@@ -1,4 +1,11 @@
+<!-- there are two type of function ..function by reference and function by value -->
+
 <?php
 
+function hello($name)
+{
+    echo "hello $name";
+}
 
-?>
+
+hello("hello world");
